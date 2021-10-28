6bc14bdfd177111c1c8b754808662aa72f5a4150
Yamin Karnu <76117511+SantaYamin@users.noreply.github.com>
2021-10-28 23:31:34 +0530

    Create randomemoji.py

---
+++ d3vilbot/plugins/randomemoji.py
@@ -0,0 +1,20 @@
+import os
+from . import *
+from ..utils import admin_cmd, sudo_cmd, edit_or_reply
+from multiutility import EmojiCreator
+
+Emoji = EmojiCreator()
+
+@borg.on(d3vil_cmd(pattern="randomoji", outgoing=True))  # pylint:disable=E0602
+@borg.on(sudo_cmd(pattern="randomoji"))
+async def _(event):
+    mmmm = await edit_or_reply(event, "**Generating Your Random Emoji ⏰✍️**")
+    emoji = Emoji.get_random()
+    await event.respond("**--- Random Emoji For You ---**", file=emoji)
+    os.remove(emoji)
+    await mmmm.delete()
+ 
+ 
+CmdHelp("randomemoji").add_command(
+  "randomoji", None, "Get Random Emoji as Image."
+).add()
