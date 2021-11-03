faa23e2da72a688daf10dba1b0c1b489ce0b56f6
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-11-03 16:11:38 +0530

    Create othervars.py

---
+++ d3vilbot/assistant/othervars.py
@@ -0,0 +1,193 @@
+from . import *
+
+# main menuu for setting other vars
+
+
+@callback("otvars")
+@owner
+async def otvaar(event):
+    await event.edit(
+        "Other Variables to set for @D3VIL_BOT_SUPPORT:",
+        buttons=[
+            [Button.inline("Tᴀɢ Lᴏɢɢᴇʀ", data="taglog")],
+            [Button.inline("SᴜᴘᴇʀFʙᴀɴ", data="sfban")],
+            [
+                Button.inline("Sᴜᴅᴏ Mᴏᴅᴇ", data="sudo"),
+                Button.inline("Hᴀɴᴅʟᴇʀ", data="hhndlr"),
+            ],
+            [
+                Button.inline("Exᴛʀᴀ Pʟᴜɢɪɴs", data="plg"),
+                Button.inline("Aᴅᴅᴏɴs", data="eaddon"),
+            ],
+            [Button.inline("« Bᴀᴄᴋ", data="setter")],
+        ],
+    )
+
+
+@callback("plg")
+@owner
+async def pluginch(event):
+    await event.delete()
+    pru = event.sender_id
+    var = "PLUGIN_CHANNEL"
+    name = "Plugin Channel"
+    async with event.client.conversation(pru) as conv:
+        await conv.send_message(
+            "Send id or username of a channel from where u want to install all plugins\n\nOur Channel~ @ultroidplugins\n\nUse /cancel to cancel."
+        )
+        response = conv.wait_event(events.NewMessage(chats=pru))
+        response = await response
+        themssg = response.message.message
+        if themssg == "/cancel":
+            return await conv.send_message("Cancelled!!")
+        else:
+            await setit(event, var, themssg)
+            await conv.send_message(
+                "{} changed to {}\n After Setting All Things Do Restart".format(
+                    name, themssg
+                )
+            )
+
+
+@callback("hhndlr")
+@owner
+async def hndlrr(event):
+    await event.delete()
+    pru = event.sender_id
+    var = "HANDLER"
+    name = "Handler/ Trigger"
+    async with event.client.conversation(pru) as conv:
+        await conv.send_message(
+            f"Send The Symbol Which u want as Handler/Trigger to use bot\nUr Current Handler is [ `{HANDLER}` ]\n\n use /cancel to cancel."
+        )
+        response = conv.wait_event(events.NewMessage(chats=pru))
+        response = await response
+        themssg = response.message.message
+        if themssg == "/cancel":
+            return await conv.send_message("Cancelled!!")
+        else:
+            await setit(event, var, themssg)
+            await conv.send_message("{} changed to {}".format(name, themssg))
+
+
+@callback("taglog")
+@owner
+async def tagloggerr(event):
+    await event.delete()
+    pru = event.sender_id
+    var = "TAG_LOG"
+    name = "Tag Log Group"
+    async with event.client.conversation(pru) as conv:
+        await conv.send_message(
+            f"Make a group, add your assistant and make it admin.\nGet the `{hndlr}id` of that group and send it here for tag logs.\n\nUse /cancel to cancel."
+        )
+        response = conv.wait_event(events.NewMessage(chats=pru))
+        response = await response
+        themssg = response.message.message
+        if themssg == "/cancel":
+            return await conv.send_message("Cancelled!!")
+        else:
+            await setit(event, var, themssg)
+            await conv.send_message("{} changed to {}".format(name, themssg))
+
+
+@callback("eaddon")
+@owner
+async def pmset(event):
+    await event.edit(
+        "ADDONS~ Extra Plugins:",
+        buttons=[
+            [Button.inline("Aᴅᴅᴏɴs  Oɴ", data="edon")],
+            [Button.inline("Aᴅᴅᴏɴs  Oғғ", data="edof")],
+            [Button.inline("« Bᴀᴄᴋ", data="otvars")],
+        ],
+    )
+
+
+@callback("edon")
+@owner
+async def eddon(event):
+    var = "EXTRA_REPO"
+    await setit(event, var, "True")
+    await event.edit(
+        "Done! ADDONS has been turned on!!\n\n After Setting All Things Do Restart"
+    )
+
+
+@callback("edof")
+@owner
+async def eddof(event):
+    var = "EXTRA_REPO"
+    await setit(event, var, "False")
+    await event.edit(
+        "Done! ADDONS has been turned off!! After Setting All Things Do Restart"
+    )
+
+
+@callback("sudo")
+@owner
+async def pmset(event):
+    await event.edit(
+        f"SUDO MODE ~ Some peoples can use ur Bot which u selected. To know More use `{HNDLR}help sudo`",
+        buttons=[
+            [Button.inline("Sᴜᴅᴏ Mᴏᴅᴇ  Oɴ", data="onsudo")],
+            [Button.inline("Sᴜᴅᴏ Mᴏᴅᴇ  Oғғ", data="ofsudo")],
+            [Button.inline("« Bᴀᴄᴋ", data="otvars")],
+        ],
+    )
+
+
+@callback("onsudo")
+@owner
+async def eddon(event):
+    var = "SUDO"
+    await setit(event, var, "True")
+    await event.edit(
+        "Done! SUDO MODE has been turned on!!\n\n After Setting All Things Do Restart"
+    )
+
+
+@callback("ofsudo")
+@owner
+async def eddof(event):
+    var = "SUDO"
+    await setit(event, var, "False")
+    await event.edit(
+        "Done! SUDO MODE has been turned off!! After Setting All Things Do Restart"
+    )
+
+
+@callback("sfban")
+@owner
+async def sfban(event):
+    await event.edit(
+        "SuperFban Settings:",
+        buttons=[
+            [Button.inline("FBᴀɴ Gʀᴏᴜᴘ", data="sfgrp")],
+            [Button.inline("Exᴄʟᴜᴅᴇ Fᴇᴅs", data="sfexf")],
+            [Button.inline("« Bᴀᴄᴋ", data="otvars")],
+        ],
+    )
+
+
+@callback("sfgrp")
+@owner
+async def sfgrp(event):
+    await event.delete()
+    name = "FBan Group ID"
+    var = "FBAN_LOG_GROUP"
+    pru = event.sender_id
+    async with asst.conversation(pru) as conv:
+        await conv.send_message(
+            f"Make a group, add @MissRose_Bot, send `{hndlr}id`, copy that and send it here.\nUse /cancel to go back."
+        )
+        response = conv.wait_event(events.NewMessage(chats=pru))
+        response = await response
+        themssg = response.message.message
+        if themssg == "/cancel":
+            return await conv.send_message("Cancelled!!")
+        else:
+            await setit(event, var, themssg)
+            await conv.send_message("{} changed to {}".format(name, themssg))
+
+
